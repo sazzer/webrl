8926c2efd92461b2250362808ef4d4363d45ce51
Graham Cox <graham.cox@oclc.org>
2014-01-29 13:40:45 +0000

    Actually load the configuration from the filesystem. This uses promises in a recursive manner so be careful, but it works and isn't hard to read.

--- src/routes/yui.js
+++ src/routes/yui.js
@@ -2,29 +2,42 @@
  * GET Yui3 Loader Configuration
  */
 var fs = require('fs'),
+    util = require('util'),
     Q = require('q'),
     path = require('path');
 
 exports.config = function(req, res) {
-    var modules = {
-        'webrl-app': {
-            requires: [
-                'app',
-                'webrl-main-view'
-            ],
-            lang: ['en']
-        },
-        'webrl-main-view': {
-            requires: [
-                'view'
-            ],
-            lang: ['en']
-        }
-    };
-    res.render('yui/config', {
-        group: 'webrl',
-        modules: modules
-    });
+    var modulesPath = path.join(__dirname, '../yui');
+
+    console.log("Loading from " + modulesPath);
+    Q.nfcall(fs.readdir, modulesPath)
+        .then(function(modules) {
+            return Q.all(modules.map(function(module) {
+                var moduleData = path.join(modulesPath, module, module + ".json");
+
+                return Q.nfcall(fs.readFile, moduleData)
+                    .then(JSON.parse)
+                    .then(function(config) {
+                        config.name = module;
+                        return config;
+                    });
+            }));
+        }).then(function(modules) {
+            var config = {};
+            modules.forEach(function(module) {
+                config[module.name] = module;
+            });
+            return config;
+        }).then(function(modules) {
+            res.type('text/javascript');
+            res.render('yui/config', {
+                group: 'webrl',
+                modules: modules
+            });
+        }, function(error) {
+            console.log(error);
+            res.send(404, "Failed to load module: " + moduleName);
+        });
 };
 
 exports.module = function(req, res) {

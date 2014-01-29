c61425673a04f5a32b0bf553ec8d10b6d6c05452
Graham Cox <graham@grahamcox.co.uk>
2014-01-29 19:50:40 +0000

    Move the logic for loading YUI modules to its own class, so that it can be tested easier.

---
+++ src/main/yui.js
@@ -0,0 +1,74 @@
+var fs = require('fs'),
+    util = require('util'),
+    Q = require('q'),
+    path = require('path')
+    Base = require('selfish').Base;
+
+module.exports = Base.extend({
+    /**
+     * Initialize the object
+     * @param base {Path} The base for the YUI Modules
+     */
+    initialize: function(base) {
+        this._base = base;
+        this._config = this._generateConfig(base);
+    },
+    /**
+     * Get the YUI Configuration that we've generated
+     * @return {Q} A Promise of the YUI Configuration to use
+     */
+    config: function() {
+        return this._config;
+    },
+    /**
+     * Get the actual module configuration for the module with the given name
+     * @param moduleName {String} the name of the module to get
+     * @return {Q} A Promise of the module details to use
+     */
+    module: function(moduleName) {
+        var modulePath = path.join(this._base, moduleName),
+            moduleScript = path.join(modulePath, moduleName + '.js'),
+            moduleData = path.join(modulePath, moduleName + '.json');
+        return Q.all([
+            Q.nfcall(fs.readFile, moduleScript),
+            Q.nfcall(fs.readFile, moduleData)
+                .then(JSON.parse)
+        ]).spread(function(moduleContents, moduleConfig) {
+            return {
+                name: moduleName,
+                version: '1.0.0',
+                contents: moduleContents,
+                config: moduleConfig
+            };
+        });
+    },
+    /**
+     * Generate the YUI Configuration
+     * @param modulesPath {Path} The base path to the YUI modules
+     * @return {Q} A Promise of the YUI Configuration to use
+     */
+    _generateConfig: function(modulesPath) {
+        console.log("Loading YUI Modules from: " + modulesPath);
+        return Q.nfcall(fs.readdir, modulesPath)
+            .then(function(modules) {
+                return Q.all(modules.map(function(module) {
+                    console.log("Loading module: " + module);
+                    var moduleData = path.join(modulesPath, module, module + ".json");
+
+                    return Q.nfcall(fs.readFile, moduleData)
+                        .then(JSON.parse)
+                        .then(function(config) {
+                            config.name = module;
+                            return config;
+                        });
+                }));
+            }).then(function(modules) {
+                var config = {};
+                modules.forEach(function(module) {
+                    config[module.name] = module;
+                });
+                return config;
+            });
+    }
+});
+
--- src/routes/yui.js
+++ src/routes/yui.js
@@ -1,63 +1,29 @@
 /*
  * GET Yui3 Loader Configuration
  */
-var fs = require('fs'),
-    util = require('util'),
-    Q = require('q'),
-    path = require('path');
+
+var YUI = require('../main/yui'),
+    path = require('path'),
+    modulesPath = path.join(__dirname, '../yui'),
+    yui = YUI.new(modulesPath);
 
 exports.config = function(req, res) {
-    var modulesPath = path.join(__dirname, '../yui');
-
-    console.log("Loading from " + modulesPath);
-    Q.nfcall(fs.readdir, modulesPath)
-        .then(function(modules) {
-            return Q.all(modules.map(function(module) {
-                var moduleData = path.join(modulesPath, module, module + ".json");
-
-                return Q.nfcall(fs.readFile, moduleData)
-                    .then(JSON.parse)
-                    .then(function(config) {
-                        config.name = module;
-                        return config;
-                    });
-            }));
-        }).then(function(modules) {
-            var config = {};
-            modules.forEach(function(module) {
-                config[module.name] = module;
-            });
-            return config;
-        }).then(function(modules) {
-            res.type('text/javascript');
-            res.render('yui/config', {
-                group: 'webrl',
-                modules: modules
-            });
-        }, function(error) {
-            console.log(error);
-            res.send(404, "Failed to load module: " + moduleName);
+    yui.config().then(function(modules) {
+        res.type('text/javascript');
+        res.render('yui/config', {
+            group: 'webrl',
+            modules: modules
         });
+    }, function(error) {
+        console.log(error);
+        res.send(404, "Failed to load module: " + moduleName);
+    });
 };
 
 exports.module = function(req, res) {
-    var moduleName = req.params.module,
-        modulePath = path.join(__dirname, '../yui', moduleName),
-        moduleScript = path.join(modulePath, moduleName + '.js'),
-        moduleData = path.join(modulePath, moduleName + '.json');
-        
-    Q.all([
-        Q.nfcall(fs.readFile, moduleScript),
-        Q.nfcall(fs.readFile, moduleData)
-            .then(JSON.parse)
-    ]).spread(function(moduleContents, moduleConfig) {
+    yui.module(req.params.module).then(function(module) {
         res.type('text/javascript');
-        res.render('yui/module', {
-            name: moduleName,
-            version: '1.0.0',
-            contents: moduleContents,
-            config: moduleConfig
-        });
+        res.render('yui/module', module);
     }, function(error) {
         console.log(error);
         res.send(404, "Failed to load module: " + moduleName);

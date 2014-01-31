47452166c7c7e37255dc5b621dac8e7cb442b4f6
Graham Cox <graham@grahamcox.co.uk>
2014-01-31 12:21:25 +0000

    Actual real page with i18n working and actually doing something.

--- src/yui/webrl-app/webrl-app.js
+++ src/yui/webrl-app/webrl-app.js
@@ -1,1 +1,18 @@
-Y.namespace("WebRL").App = undefined;
+Y.namespace("WebRL").App = Y.Base.create("webrl-app", Y.Base, [
+        // Extensions
+    ], {
+        // Prototype
+        initializer: function() {
+            var strings = this.get("strings"),
+                title = strings.title;
+            Y.log("Initializing the application: " + title);
+            document.title = title;
+        }
+    }, {
+        // Statics
+        ATTRS: {
+            strings: {
+                value: Y.Intl.get("webrl-app")
+            }
+        }
+});

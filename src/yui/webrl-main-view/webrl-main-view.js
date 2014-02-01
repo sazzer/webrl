6063eb1b9aa9ecef00a1b954817e2a458d3e103a
Graham Cox <graham@grahamcox.co.uk>
2014-02-01 18:05:59 +0000

    Actually lay out the screen in a RL manner

--- src/yui/webrl-main-view/webrl-main-view.js
+++ src/yui/webrl-main-view/webrl-main-view.js
@@ -4,12 +4,10 @@ Y.namespace("WebRL").MainView = Y.Base.create("webrl-main-view", Y.WebRL.Templat
         // Prototype
         template: [
             "<div class='main-view'>",
-                "<div class='header'>",
-                "</div>",
-                "<div class='body'>",
-                "</div>",
-                "<div class='footer'>",
-                "</div>",
+                "<div class='map'></div>",
+                "<div class='sidebar'></div>",
+                "<div class='messages'></div>",
+                "<div class='statusbar'></div>",
             "</div>"].join("")
     }, {
         // Statics
